2b6ddc352429b25f35b78e06e0bbb8f59747bdf6
andrew <andrew901@sjccs142.saintjoe.edu>
2015-01-19 12:51:10 -0500

    New loop!

---
+++ looptri.js
@@ -0,0 +1,9 @@
+var output = "#";
+var count = 1;
+for(var i = 0; i < 7; i++){	//loops for each row
+		var j = 0;
+		    while(j < count){	//loops to output each row
+			      		print(output);	
+					    }
+		      	println('');	//start new line for a new row
+}
