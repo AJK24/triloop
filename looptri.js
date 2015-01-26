02649f041036ad79731cdaf1dfac7da043add09b
Andrew <akv9509@saintjoe.edu>
2015-01-26 12:13:05 -0500

    Updated with comments

--- looptri.js
+++ looptri.js
@@ -1,12 +1,15 @@
+//Andrew Klump
+//Chapter 2 - making 
 
-
+//this is allowed for the print fucntion
 var print = function(args){
 	console.log(args);
 }
 
-var string = "";
-var output = "#";
-var count = 1;
+
+var string = ""; //the string that will be added to and printed
+var output = "#";	//the simple hashtag that will be added to the string 
+var count = 1;	/// The count is needed to see how many times each is used
 for(var i = 0; i < 7; i++){	//loops for each row
    string += output
     
